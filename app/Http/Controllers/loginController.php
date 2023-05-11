aeb4a8d8eb39afd85a85528f56dd9df71a07c0d9
Nova <azzasekarnovaa@gmail.com>
2023-05-11 15:36:44 +0800

    add login and register controller

---
+++ app/Http/Controllers/loginController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class loginController extends Controller
+{
+    public function dologin(Request $r)
+    {
+
+        $systemAdmin = array(
+            'username' => $r->username,
+            'password' => $r->password,
+            'role' => 1,
+            'status' => 0
+        );
+        $customer = array(
+            'username' => $r->username,
+            'password' => $r->password,
+            'role' => 2,
+            'status' => 0
+        );
+
+        $owner = array(
+            'username' => $r->username,
+            'password' => $r->password,
+            'role' => 3,
+            'status' => 0
+        );
+
+        $manager = array(
+            'username' => $r->username,
+            'password' => $r->password,
+            'role' => 4,
+            'status' => 0
+        );
+
+
+
+        if (Auth::attempt($systemAdmin)) {
+            return redirect()->route('admin-dashboard');
+        }
+        else if(Auth::attempt($customer)){
+//            dd(Auth::check());
+            return redirect()->route('index');
+        }
+        else {
+            return redirect()->back()->withErrors(['wrong']);
+        }
+    }
+    public function login()
+    {
+        return view('web.Login');
+    }
+}
---
+++ app/Http/Controllers/registerController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class registerController extends Controller
+{
+    //USER
+    public function registerAll(Request $r)
+    {
+        //Apabila sudah ada di database
+        if (\App\Models\users::where("email", $r->email)->where('status', 0)->first()) {
+            return redirect()->back()->with('error', 'email exists'); //kembali tanpa menyimpan apa apa di database
+        }
+        //jika belum ada lakukan penambahan data di database
+        else {
+            $temp = new \App\Models\users;
+            $temp->role =$r->role;
+//            $temp->id_location = $r->id_location;
+            $temp->name = $r->name;
+            $temp->username = $r->username;
+            $temp->email = $r->email;
+            $password = $r->password;
+            $temp->password = bcrypt($password);
+            $date = date_default_timezone_set('Asia/Singapore');
+            $temp->created_at = date('Y-m-d H:i:s');
+            $temp->updated_at =  date('Y-m-d H:i:s');
+            $temp->save();
+            return redirect()->back()->with('success', 'Registered');
+        }
+    }
+    public function signup()
+    {
+        return view('web.SignUpTixID');
+    }
+
+}
